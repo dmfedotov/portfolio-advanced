6c05c507e8c8d390260406f0486c94644726c4ea
Dima Fedotov <crismsfiva@gmail.com>
2017-09-11 12:50:08 +0300

    верстка index страницы

---
+++ gulpfile.js
@@ -0,0 +1,162 @@
+const gulp = require('gulp');
+const del = require('del');
+const browserSync = require('browser-sync').create();
+const pug = require('gulp-pug');
+const autoprefixer = require('gulp-autoprefixer');
+const svgSprite = require('gulp-svg-sprites');
+const svgmin = require('gulp-svgmin');
+const cheerio = require('gulp-cheerio');
+const replace = require('gulp-replace');
+
+// styles 
+const sass = require('gulp-sass');
+const rename = require('gulp-rename');
+const sourcemaps = require('gulp-sourcemaps');
+
+// scripts
+const gulpWebpack = require('gulp-webpack');
+const webpack = require('webpack');
+const webpackConfig = require('./webpack.config.js')
+
+const paths = {
+  root: './docs',
+  styles: {
+    src: 'src/styles/**/*.scss',
+    dest: 'docs/assets/styles/'
+  },
+  scripts: {
+    src: 'src/scripts/**/*.js',
+    dest: 'docs/assets/scripts/'
+  },
+  templates: {
+    src: 'src/templates/',
+    dest: 'docs/assets/'
+  },
+  images: {
+    src: 'src/images/**/*.{jpg,svg,png}',
+    dest: 'docs/assets/images/'
+  },
+  icons: {
+    src: 'src/images/icons/*.svg',
+    dest: 'src/images/icons/'
+  },
+  fonts: {
+    src: 'src/fonts/**/*.*',
+    dest: 'docs/assets/fonts/'
+  }
+};
+
+// pug
+function html() {
+  return gulp.src(paths.templates.src + "pages/*.pug")
+    .pipe(pug({
+      pretty: true
+    }))
+    .pipe(gulp.dest(paths.root));
+}
+
+// scss
+function styles() {
+  return gulp.src('./src/styles/app.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass({
+      outputStyle: 'compressed'
+    }))
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
+    }))
+    .pipe(sourcemaps.write())
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(gulp.dest(paths.styles.dest))
+}
+
+// перенос картинок
+function images() {
+  return gulp.src(paths.images.src)
+    .pipe(gulp.dest(paths.images.dest));
+}
+
+// перенос шрифтов
+function fonts() {
+  return gulp.src(paths.fonts.src)
+    .pipe(gulp.dest(paths.fonts.dest));
+}
+
+// webpack
+function scripts() {
+  return gulp.src('src/scripts/app.js')
+    .pipe(gulpWebpack(webpackConfig, webpack))
+    .pipe(gulp.dest(paths.scripts.dest));
+}
+
+// очистка папки docs
+function clean() {
+  return del(paths.root);
+}
+
+// следим за src и запускаем нужные таски (компиляция и пр.)
+function watch() {
+  gulp.watch(paths.scripts.src, scripts);
+  gulp.watch(paths.styles.src, styles);
+  gulp.watch(paths.templates.src, html);
+  gulp.watch(paths.images.src, images);
+  gulp.watch(paths.fonts.src, fonts);
+}
+
+// следим за docs и релоадим браузер
+function server() {
+  browserSync.init({
+    server: paths.root,
+    reloadDelay: 200
+  });
+  browserSync.watch(paths.root + '/**/*.{html,css}', browserSync.reload);
+}
+
+// svg спрайт
+function sprite() {
+  return gulp.src(paths.icons.src)
+    .pipe(svgmin({
+      js2svg: {
+        pretty: true
+      }
+    }))
+    .pipe(cheerio({
+      run: function ($) {
+        $('[fill]').removeAttr('fill');
+        $('[stroke]').removeAttr('stroke');
+        $('[style]').removeAttr('style');
+      },
+      parserOptions: {
+        xmlMode: true
+      }
+    }))
+    .pipe(replace('&gt;', '>'))
+    .pipe(svgSprite({
+      mode: "symbols",
+      svg: {
+        symbols: 'sprite.svg'
+      }
+    }))
+    .pipe(gulp.dest(paths.icons.dest));
+}
+
+// экспорт функциq для доступа из терминала
+exports.clean = clean;
+exports.styles = styles;
+exports.scripts = scripts;
+exports.templates = html;
+exports.images = images;
+exports.watch = watch;
+exports.server = server;
+exports.fonts = fonts;
+exports.sprite = sprite;
+
+// сборка и слежка
+gulp.task('default', gulp.series(
+  clean,
+  gulp.parallel(styles, scripts, html, images, fonts),
+  gulp.parallel(watch, server)
+));
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const webpack = require('webpack'); 
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+
+const config = {
+  output: {
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new UglifyJSPlugin({
+      sourceMap: true
+    })
+  ],
+  module: {
+    rules: [
+      {
+        enforce: "pre",
+        loader: "eslint-loader",
+        options: {
+            fix: true
+        }            
+      },
+    ]  
+  }
+}
+
+module.exports = config;
