9ed74eec85869f152d0685d4c2cc327ceeb513c4
Dima Fedotov <crismsfiva@gmail.com>
2017-10-05 09:42:55 +0300

    добавлена валидация на главной странице и странице с работами

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -3,9 +3,47 @@ import {parallax} from './common/parallax';
 import {hamburger} from './common/hamburger';
 import {initMap} from './common/google-map';
 import Validation from './common/validation';
+import getData from './common/getData';
 
-const authForm = document.querySelector('#auth-form');
-console.log(Validation.init(authForm));
+
+if ($('#auth-form')) { 
+  var authForm = $('#auth-form');
+  Validation.init(authForm);
+  Validation.clearError(authForm);
+
+  authForm.submit(() => {
+    if(Validation.result()) {
+      getData('http://localhost:3000').then(function(response) {
+        console.log('Success!', response);
+      }, function(error) {
+        console.error('Failed!', error);
+      });
+    }
+  });
+}
+
+if (!document.querySelector('#auth-form')) { 
+  var feedbackForm = $('#feedback-form');
+  Validation.init(feedbackForm);
+  Validation.clearError(feedbackForm);
+
+  feedbackForm.submit(() => {
+    if(Validation.result()) {
+      getData('http://localhost:3000').then(function(response) {
+        console.log('Success!', response);
+      }, function(error) {
+        console.error('Failed!', error);
+      });
+    }
+  });
+}
+
+
+
+
+if (document.querySelector('#google-map')) {
+  initMap();
+}
 
 
 
--- src/scripts/common/validation.js
+++ src/scripts/common/validation.js
@@ -1,41 +1,80 @@
 export default (function () {
   var _form = null;
   var _result = false;
+  var _resultArray = [];
 
   function _init(form) {
-    console.log('validation init');
     _form = form;
     _addListeners();
   }
 
   function _addListeners() {
-    _form.addEventListener('submit', function (ev) {
+    _form.submit(function (ev) {
       ev.preventDefault();
+      _resultArray = [];
       _validate(_form, _onEmptyVariant);
+      _validate(_form, _onCheckVariant);
       _validate(_form, _onRobotVariant);
-    });
-    _form.addEventListener('reset', function () {
-      _clear(_form);
+      _isResult(_resultArray);
+      console.log(_result);
     });
   }
 
   function _validate(form, method) {
-    // console.log('submit');
     _result = true;
-    [].forEach.call(form.elements, function (elem) {
-      if (elem.tagName !== 'BUTTON') {
-        if (!method(elem)) {
-          elem.classList.add('form-error');
-          console.log('роботам тут не место');
+    let _formElems = form.find('input').add('textarea');
+
+    _formElems.each((i, elem) => {
+      if ($(elem).attr('type') == 'text'
+             ||
+          $(elem).attr('type') == 'password'
+             ||
+          $(elem).attr('type') == 'email'
+             ||
+          $(elem).is('textarea')) {
+        if (!method($(elem))) {
+          _result = false;
+        }
+      }
+
+      if ($(elem).attr('type') == 'checkbox') {
+        if (!method($(elem))) {
+          _result = false;
+        }
+      }
+
+      if ($(elem).attr('type') == 'radio') {
+        if (!method($(elem))) {
+          _result = false;
+        }
+      }
+
+      if ($(elem).attr('type') == 'email') {
+        if (!method($(elem))) {
           _result = false;
         }
       }
     });
+    _resultArray.push(_result);
   }
 
-  function _clear(form) {
-    [].forEach.call(form.elements, function (elem) {
-      elem.classList.remove('form-error');
+  function _isResult(resArray) {
+    for (var i = 0; i < resArray.length; i++) {
+      if (!resArray[i]) {
+        _result = false;
+        break;
+      } else {
+        _result = true;
+      }
+    }
+  }
+
+  function _clearError(form) {
+    var _formChildren = form.children();
+    form.on('reset', () => {
+      _formChildren.each((i, item) => {
+        $(item).removeClass('form-error');
+      });
     });
   }
 
@@ -44,19 +83,72 @@ export default (function () {
   }
 
   function _onEmptyVariant(elem) {
-    return elem.value == '' ? false : true;
+    elem.keyup(() => {
+      elem.parent().removeClass('form-error');
+    });
+
+    if (elem.attr('name') == 'login' && elem.val() == ''
+          ||
+        elem.attr('name') == 'name' && elem.val() == ''
+          ||
+        elem.attr('name') == 'password' && elem.val() == ''
+          ||
+        elem.attr('name') == 'message' && elem.val() == ''
+          ||
+        elem.attr('name') == 'email' && elem.val() == ''
+    ) {
+      elem.parent().addClass('form-error');
+      return false;
+    } else {
+      // elem.parent().removeClass('form-error');
+      elem.parent().addClass('form-success');
+      return true;
+    }
   }
 
-  function _onLengthVariant(elem) {
-    return elem.value == '' ? false : true;
-  }
+  // function _onLengthVariant(elem) {
+  //   return elem.value == '' ? false : true;
+  // }
 
   function _onRobotVariant(elem) {
-    return elem.checked ? true : false;
+    elem.click(() => {
+      if (elem.attr('id') == 'auth-radio-yes') {
+        if (elem.is(':checked')) {
+          $('.form').find('.form__line-title').removeClass('form-error');
+        }     
+      }
+    });
+
+    if (elem.attr('id') == 'auth-radio-yes' && !elem.prop('checked')
+                              ||
+        elem.attr('id') == 'auth-radio-no' && elem.prop('checked')) {
+      $('.form').find('.form__line-title').addClass('form-error');
+      return false;
+    } else {
+      $('.form').find('.form__line-title').removeClass('form-error');
+      return true;
+    }
+  }
+
+  function _onCheckVariant(elem) {
+    elem.click(() => {
+      if (elem.is(':checked')) {
+        elem.parent().parent().parent().removeClass('form-error');
+      }
+    });
+
+    if (elem.attr('id') == 'auth-check' && !elem.prop('checked')) {
+      elem.parent().parent().parent().addClass('form-error');
+      return false;
+    } else {
+      elem.parent().parent().parent().removeClass('form-error');
+      return true;
+    }
   }
 
   return {
     init: _init,
     result: _onResult,
+    clearError: _clearError,
   };
 }());
